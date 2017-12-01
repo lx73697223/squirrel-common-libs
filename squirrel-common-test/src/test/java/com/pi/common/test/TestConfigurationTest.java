a08703abf47cd48c93e879df6aea3c7cd4ab144e
xiang.li <lixiang01234567890@163.com>
2017-12-01 18:11:21 +0800

    fix package incorrect

--- squirrel-common-test/src/test/java/com/pi/common/test/TestConfigurationTest.java
+++ squirrel-common-test/src/test/java/com/pi/common/test/TestConfigurationTest.java
@@ -1,12 +1,12 @@
 package com.pi.common.test;
 
+import static org.testng.Assert.assertEquals;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
 
 @SpringBootTest(classes = TestConfig.class)
 @SpringBootConfiguration
@@ -16,8 +16,7 @@ public class TestConfigurationTest extends AbstractTestNGSpringContextTests {
     private TestProperties foo;
 
     @Test
-    public void testPlusCount()
-            throws Exception {
+    public void testPlusCount() throws Exception {
         assertEquals(foo.getName(), "from test config");
     }
 
---
+++ squirrel-common-test/src/test/java/com/pi/common/test/TestProperties.java
@@ -0,0 +1,12 @@
+package com.pi.common.test;
+
+import lombok.Data;
+
+@Data
+public class TestProperties {
+
+    private String name;
+
+    private Integer age;
+
+}
