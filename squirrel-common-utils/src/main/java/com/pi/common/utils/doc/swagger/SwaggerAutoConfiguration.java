ffd3bd20c274928a83c62fb7538ab9f0010161b3
xiang.li <lixiang01234567890@163.com>
2017-12-25 19:21:01 +0800

    add spring jdbc

--- squirrel-common-utils/src/main/java/com/pi/common/utils/doc/swagger/SwaggerAutoConfiguration.java
+++ squirrel-common-utils/src/main/java/com/pi/common/utils/doc/swagger/SwaggerAutoConfiguration.java
@@ -1,6 +1,8 @@
 package com.pi.common.utils.doc.swagger;
 
-import com.google.common.base.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -8,6 +10,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+
+import com.google.common.base.Predicate;
+
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,13 +24,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @EnableSwagger2
 @Configuration
-@ConditionalOnProperty(prefix = SwaggerProperties.PREFIX, name = "enable")
 @EnableConfigurationProperties(SwaggerProperties.class)
+@ConditionalOnProperty(prefix = SwaggerProperties.PREFIX, name = "enable")
 public class SwaggerAutoConfiguration {
 
     @Autowired
@@ -37,27 +39,23 @@ public class SwaggerAutoConfiguration {
     }
 
     protected Docket createDocket() {
-        return createDocket(pathsRegex(properties.getRegexPath()), properties.getGroupName(),
-                            properties.getScanBasePackage(), createApiInfo(),
-                            properties.getNeedToken(), properties.getEnable());
+        return createDocket(pathsRegex(properties.getRegexPath()), properties.getGroupName(), properties.getScanBasePackage(),
+                createApiInfo(), properties.getNeedToken(), properties.getEnable());
     }
 
     protected ApiInfo createApiInfo() {
-        return new ApiInfo(properties.getTitle(), properties.getDescription(),
-                           properties.getVersion(), properties.getTermsOfServiceUrl(),
-                           new Contact(properties.getContactName(), properties.getContactUrl(),
-                                       properties.getContactEmail()), properties.getLicense(),
-                           properties.getLicenseUrl());
+        return new ApiInfo(properties.getTitle(), properties.getDescription(), properties.getVersion(),
+                properties.getTermsOfServiceUrl(),
+                new Contact(properties.getContactName(), properties.getContactUrl(), properties.getContactEmail()),
+                properties.getLicense(), properties.getLicenseUrl());
     }
 
-    protected Docket createDocket(Predicate<String> pathsRegex, String groupName,
-                                  String basePackage, ApiInfo apiInfo, boolean isNeedToken,
-                                  boolean enable) {
+    protected Docket createDocket(Predicate<String> pathsRegex, String groupName, String basePackage, ApiInfo apiInfo,
+            boolean isNeedToken, boolean enable) {
         Docket docket = new Docket(DocumentationType.SWAGGER_2).enable(enable);
         docket.genericModelSubstitutes(ResponseEntity.class).useDefaultResponseMessages(true);
         docket.groupName(groupName).apiInfo(apiInfo);
-        docket.select().apis(RequestHandlerSelectors.basePackage(basePackage)).paths(pathsRegex)
-              .build();
+        docket.select().apis(RequestHandlerSelectors.basePackage(basePackage)).paths(pathsRegex).build();
         if (isNeedToken) {
             docket.globalOperationParameters(getGlobalOperationParameters());
         }
@@ -67,15 +65,13 @@ public class SwaggerAutoConfiguration {
     protected List<Parameter> getGlobalOperationParameters() {
         List<Parameter> list = new ArrayList<>();
         Parameter auth = new ParameterBuilder().name("Authorization").description("Authorization")
-                                               .modelRef(new ModelRef("string")).parameterType(
-                        "header").defaultValue("Bearer ").required(true).build();
+                .modelRef(new ModelRef("string")).parameterType("header").defaultValue("Bearer ").required(true).build();
         list.add(auth);
         return list;
     }
 
     protected Predicate<String> pathsRegex(String regexPath) {
-        return StringUtils.isBlank(regexPath) ? PathSelectors.any() : PathSelectors.regex(
-                regexPath);
+        return StringUtils.isBlank(regexPath) ? PathSelectors.any() : PathSelectors.regex(regexPath);
     }
 
 }
