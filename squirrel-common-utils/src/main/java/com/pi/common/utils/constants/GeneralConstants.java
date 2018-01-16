40e1df7cc41c04a5360c9fe4db3e9169e4899627
lixiang <lixiang01234567890@163.com>
2018-01-16 23:36:10 +0800

    update test

--- squirrel-common-utils/src/main/java/com/pi/common/utils/constants/GeneralConstants.java
+++ squirrel-common-utils/src/main/java/com/pi/common/utils/constants/GeneralConstants.java
@@ -4,4 +4,8 @@ public final class GeneralConstants {
 
     public static final long DEFAULT_USER = 0L;
 
+    public static final String Authorization_Header = "Authorization";
+
+    public static final String Authorization_Token_Prefix = "Bearer ";
+
 }
