bff55761d9167c7c8ad3161d51daf6fce87b9a5a
xiang.li <lixiang01234567890@163.com>
2018-01-26 22:39:20 +0800

    rename

--- squirrel-common-utils/src/main/java/com/pi/common/utils/constants/GeneralConstants.java
+++ squirrel-common-utils/src/main/java/com/pi/common/utils/constants/GeneralConstants.java
@@ -2,10 +2,10 @@ package com.pi.common.utils.constants;
 
 public final class GeneralConstants {
 
-    public static final long DEFAULT_USER = 0L;
+    public static final long DEFAULT_USER_ID = 0L;
 
-    public static final String Authorization_Header = "Authorization";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
 
-    public static final String Authorization_Token_Prefix = "Bearer ";
+    public static final String AUTHORIZATION_TOKEN_PREFIX = "Bearer ";
 
 }
